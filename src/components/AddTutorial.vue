dfe474b91d6e518334c786feee66e32848f0b61d
edos21 <edos21@gmail.com>
2022-03-12 21:12:19 -0300

    adds inputs starts styling

--- src/components/AddTutorial.vue
+++ src/components/AddTutorial.vue
@@ -1,32 +1,75 @@
 <template>
   <div class="submit-form">
     <div v-if="!submitted">
-      <div class="form-group">
-        <label for="title">Title</label>
-        <input
-            type="text"
-            class="form-control"
-            id="title"
-            required
-            v-model="tutorial.title"
-            name="title"
-        />
+      <div>
+        <h4>Nuevo Tutorial</h4>
+        <div class="form-group">
+          <label for="title">Titulo</label>
+          <input
+              type="text"
+              class="form-control"
+              id="title"
+              required
+              v-model="tutorial.title"
+              name="title"
+          />
+        </div>
+        <div class="form-group">
+          <label for="video_url">URL</label>
+          <input
+              type="text"
+              class="form-control"
+              id="video_url"
+              required
+              v-model="tutorial.video_url"
+              name="video_url"
+          />
+        </div>
+        <div class="form-group">
+          <label for="description">Descripci&oacute;n</label>
+          <textarea
+              class="form-control"
+              id="description"
+              required
+              v-model="tutorial.description"
+              name="description"
+          />
+        </div>
+        <label class="block-input">¿Como quiere mantener el tutorial?</label>
+        <div class="form-group form-check-inline block">
+          <div class="form-check">
+            <input
+                class="form-check-input"
+                type="radio"
+                name="published_status"
+                v-model="tutorial.published_status"
+                value="false">
+            <label class="form-check-label">
+              Oculto
+            </label>
+          </div>
+          <div class="form-check">
+            <input
+                class="form-check-input"
+                type="radio"
+                name="published_status"
+                v-model="tutorial.published_status"
+                value="true">
+            <label class="form-check-label">
+              Publico
+            </label>
+          </div>
+        </div>
+        <button @click="saveTutorial" class="btn btn-success block-input">Agregar</button>
       </div>
-      <div class="form-group">
-        <label for="description">Description</label>
-        <input
-            class="form-control"
-            id="description"
-            required
-            v-model="tutorial.description"
-            name="description"
-        />
-      </div>
-      <button @click="saveTutorial" class="btn btn-success">Submit</button>
     </div>
     <div v-else>
-      <h4>You submitted successfully!</h4>
-      <button class="btn btn-success" @click="newTutorial">Add</button>
+      <div class="card text-center">
+        <div class="card-body">
+          <h4>Tutorial agregado correctamente!</h4>
+          <button class="btn btn-success" @click="newTutorial">Agregar otro tutorial</button>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -40,6 +83,7 @@ export default {
         id: null,
         title: "",
         description: "",
+        video_url: "",
         published_status: false
       },
       submitted: false
@@ -47,9 +91,11 @@ export default {
   },
   methods: {
     saveTutorial() {
-      var data = {
+      const data = {
         title: this.tutorial.title,
-        description: this.tutorial.description
+        description: this.tutorial.description,
+        video_url: this.tutorial.video_url,
+        published_status: this.tutorial.published_status
       };
       TutorialDataService.create(data)
           .then(response => {
@@ -71,7 +117,17 @@ export default {
 </script>
 <style>
 .submit-form {
-  max-width: 300px;
+  max-width: 600px;
+  border-radius: 5px;
+  padding: 20px;
   margin: auto;
+  background: #cdd5dd;
+}
+.block-input {
+  display: block;
+}
+.card{
+  background: none;
+  border: none;
 }
 </style>
--- src/components/TutorialsList.vue
+++ src/components/TutorialsList.vue
@@ -1,51 +1,63 @@
 <template>
+  <div v-if="!tutorials || tutorials.length">
   <div class="list row">
-    <div class="col-md-8">
-      <div class="input-group mb-3">
-        <input type="text" class="form-control" placeholder="Search by title"
+    <div class="col-md-12">
+      <div class="input-group mb-5 mt-3">
+        <input type="text" class="form-control" placeholder="Buscar tutorial"
                v-model="title"/>
         <div class="input-group-append">
           <button class="btn btn-outline-secondary" type="button"
                   @click="searchTitle"
           >
-            Search
+            Buscar
           </button>
         </div>
       </div>
     </div>
-    <div class="col-md-6">
-      <h4>Tutorials List</h4>
-      <ul class="list-group">
-        <li class="list-group-item"
-            :class="{ active: index == currentIndex }"
-            v-for="(tutorial, index) in tutorials"
-            :key="index"
-            @click="setActiveTutorial(tutorial, index)"
-        >
-          {{ tutorial.title }}
-        </li>
-      </ul>
-      <button class="m-3 btn btn-sm btn-danger" @click="removeAllTutorials">
-        Remove All
-      </button>
-    </div>
-    <div class="col-md-6">
-      <div v-if="currentTutorial">
-        <h4>Tutorial</h4>
-        <div>
-          <label><strong>Title:</strong></label> {{ currentTutorial.title }}
-        </div>
-        <div>
-          <label><strong>Description:</strong></label> {{ currentTutorial.description }}
-        </div>
-        <div>
-          <label><strong>Status:</strong></label> {{ currentTutorial.published_status ? "Published" : "Pending" }}
-        </div>
-        <router-link :to="'/tutorials/' + currentTutorial.id" class="badge badge-warning">Edit</router-link>
+      <div class="col-md-4">
+        <h4 class="mb-4">Lista de tutoriales</h4>
+        <ul class="list-group">
+          <li class="list-group-item"
+              :class="{ active: index == currentIndex }"
+              v-for="(tutorial, index) in tutorials"
+              :key="index"
+              @click="setActiveTutorial(tutorial, index)"
+          >
+            {{ tutorial.title }}
+          </li>
+        </ul>
+        <button class="m-3 btn btn-sm btn-danger" @click="removeAllTutorials">
+          Remove All
+        </button>
       </div>
-      <div v-else>
-        <br />
-        <p>Please click on a Tutorial...</p>
+      <div class="col-md-8">
+        <div v-if="currentTutorial">
+          <h4>Tutorial</h4>
+          <div>
+            <label><strong>Title:</strong></label> {{ currentTutorial.title }}
+          </div>
+          <div>
+            <label><strong>Description:</strong></label> {{ currentTutorial.description }}
+          </div>
+          <div>
+            <label><strong>Status:</strong></label> {{ currentTutorial.published_status ? "Published" : "Pending" }}
+          </div>
+          <router-link :to="'/tutorials/' + currentTutorial.id" class="badge badge-warning">Edit</router-link>
+        </div>
+        <div v-else>
+          <h4 class="mb-4">Tutorial</h4>
+          <div class="alert alert-primary" role="alert">
+            <p>Seleccionar un tutorial para ver su contenido...</p>
+          </div>
+        </div>
+      </div>
+  </div>
+  </div>
+  <div v-else>
+    <div class="card text-center">
+      <div class="card-body">
+        <h4 class="card-title">No hay tutoriales aqui</h4>
+        <p class="card-text">Comencemos por <router-link to="/add">agregar</router-link> uno</p>
       </div>
     </div>
   </div>
@@ -113,7 +125,7 @@ export default {
 <style>
 .list {
   text-align: left;
-  max-width: 750px;
+  max-width: 1050px;
   margin: auto;
 }
 </style>
